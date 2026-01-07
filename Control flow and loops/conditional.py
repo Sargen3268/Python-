606b3b82b0000563f836d5262d14baaca180b67d
chinmaychiplunkar573-stack <chinmaychiplunkar573@gmail.com>
2026-01-07 18:57:54 +0530

    if else

---
+++ Control flow and loops/conditional.py
@@ -0,0 +1,10 @@
+age=int(input("Enter your age: "))
+
+if(age > 18 ):
+    print("you can drive")
+elif(age==18):
+    print("we will think about it")
+elif(age==0):
+    print("Oh we are smarty among us")
+else:
+    print("you cannot drive")
