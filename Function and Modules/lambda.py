51a25e3f0e96db60240bd1eb0237665da1842ef7
Chinmay <chinmaychiplunkar3368@gmail.com>
2026-01-10 13:08:43 +0530

    lambda functions

---
+++ Function and Modules/lambda.py
@@ -0,0 +1,17 @@
+square = lambda x: x*x
+
+'''
+as good as writing 
+def square(x):
+    return x*x
+'''
+sum = lambda x,y: x+y
+
+'''
+Lamda is as good as wrinting a function for add or mul but in 1 line 
+def add(x,y):
+    return x+y
+'''
+
+print(sum(35,87))
+print(square(67)) 
