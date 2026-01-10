7128585147bfd6815ac3a95c6eab0cc3608ddb0e
Chinmay <chinmaychiplunkar3368@gmail.com>
2026-01-10 12:58:10 +0530

    basic application of function

---
+++ Function and Modules/function2.py
@@ -0,0 +1,8 @@
+def average(a,b,c):
+    d = (a + b + c) / 3
+    print(d)
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+average(a,b,c)
---
+++ Function and Modules/functions.py
@@ -0,0 +1,21 @@
+# a=3
+# b=4
+# c=5
+# average = (a+b+c)/3
+
+# print(average)
+
+def average(a,b,c):
+    d = (a + b + c) / 3
+    #print(d)
+    return d
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# c = int(input("Enter third number: "))
+# average(a,b,c)
+
+o1 = average(3,4,5)
+o2 =average(1,5,6)
+
+print(o1)
+print(o2)
