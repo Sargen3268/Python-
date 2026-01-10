c1acb9e309281044e3cd24dcd91266de293b58ac
Chinmay <chinmaychiplunkar3368@gmail.com>
2026-01-10 13:22:25 +0530

    recurssion

---
+++ Function and Modules/recurrsion.py
@@ -0,0 +1,20 @@
+def fib(n):
+    #base case of recurssion 
+    if(n==0 or n==1):
+        return n
+    else:
+        return fib(n-2) + fib(n-1)
+ print(fib(4))
+
+
+# n=0
+# m = int(input("Enter a number: "))
+
+# while(fib(n)<=m):
+#     print(fib(n))
+#     n=n+1
+
+'''
+i tried using while loop to print fibonacci series till a desired number 
+it was a sucess
+'''
