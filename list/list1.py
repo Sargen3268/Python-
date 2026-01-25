ee534d3ad7da5ad739d6811f8afc17e302072efc
Chinmay <chinmaychiplunkar3368@gmail.com>
2026-01-25 20:01:41 +0530

    list

--- list/list1.py
+++ list/list1.py
@@ -1,5 +1,7 @@
 num = [10 , 20, 30, 40, 50]
-sit = [43 , "Hello" , 85 ,24 , 95 ]
+sit = [43 , "Hello" , False , 2.4 , 95 ]
 
-print(num[2])
-print(sit)
+print(num[2:5])
+print(sit [4:5])
+print(sit [2])
+print(sit [2:5])
