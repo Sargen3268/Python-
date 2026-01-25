68e6f1c748c3107e46fe69638923a1fa84c2ec5f
Chinmay <chinmaychiplunkar3368@gmail.com>
2026-01-25 20:05:56 +0530

    list

---
+++ list/listmethod.py
@@ -0,0 +1,9 @@
+marks = [85, 90, 78, 92, 88]
+print(marks)
+#marks.append(63) #adds the number in list
+#marks.pop() #removes last number if the index is not specified
+print(marks)
+
+extra_mark = [55, 67, 89]
+marks.extend(extra_mark) #adds the list in another list
+print(marks)
