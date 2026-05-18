8fa50298ecdb0765bc51f4f7801b0d56e1c3fe81
Chinmay <chinmaychiplunkar3368@gmail.com>
2026-05-18 16:00:12 +0530

    static

--- advanceconcept/static_class.py
+++ advanceconcept/static_class.py
@@ -7,11 +7,27 @@ class Employye:
     def print_info(self):
         info = f"{self.name} works in {self.company} and earns {self.salary}"
         print(info)
+    
+    
+    @staticmethod
+    def sum(a,b):
+        return a+b
+    @classmethod
+    def print_company(cls):
+        print(cls.company)
+    @classmethod
+    def change_company(cls,new_company):
+        cls.company=new_company
+
 e1=Employye("John Doe",50000)
 e2=Employye("Jane Doe",60000)
 
 
 # print(Employye.company)
 
-e1.print_info()
-e2.print_info()
+# e1.print_info()
+# e2.print_info()
+# print(e2.sum(5,23))
+e1.print_company()
+e1.change_company("Microsoft")
+e1.print_company()
