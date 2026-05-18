38a796264bd3d80d561c8011f531986506122ed7
Chinmay <chinmaychiplunkar3368@gmail.com>
2026-05-18 15:27:58 +0530

    b

--- advanceconcept/3.py
+++ advanceconcept/3.py
@@ -2,8 +2,10 @@ class Employee:
     def __init__(self, name, salary):
         self.name = name
         self.salary = salary
-    
+    def first_name(self):
+        l= self.name.split(" ")[0]
+        
+        return l
 
 e=Employee("John Doe", 50000)
-e.project = 6
-print(e.project)
+print(e.first_name())
