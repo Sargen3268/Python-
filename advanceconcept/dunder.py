860db463185987725c00e6c7be689ff6ea45b5f9
Chinmay <chinmaychiplunkar3368@gmail.com>
2026-05-20 10:19:58 +0530

    repr

--- advanceconcept/dunder.py
+++ advanceconcept/dunder.py
@@ -7,6 +7,9 @@ class Employye:
     def __str__(self):
         return f"{self.name} works in {self.company} and earns {self.salary}"
 
+    def __repr__(self):
+        return f"name : {self.name}\n salary : {self.salary}"
 e=Employye( "John Doe",50000)
 print(e.name, e.salary)
 print(str(e))
+print(repr(e))
